4f15ce3794ff919c06a1619617796a54b6e3bcb0
Lalu Arman Rozika <51574842+arman-2359media@users.noreply.github.com>
2019-06-16 19:31:27 +0700

    react build

---
+++ precache-manifest.d5f2905053f94ebc7412194e79d1a4d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be8882bb9bb8e3373cabf10d7f9c448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd149b1252989982d85c",
+    "url": "/static/css/main.7ae3c4b5.chunk.css"
+  },
+  {
+    "revision": "340abe1a09b133063693",
+    "url": "/static/js/2.b0a1aa7d.chunk.js"
+  },
+  {
+    "revision": "fd149b1252989982d85c",
+    "url": "/static/js/main.7dfc0dd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
